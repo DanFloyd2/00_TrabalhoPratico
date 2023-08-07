3ae845bdb16374e8d9915b1f836394cf374538f2
DanFloyd2 <danilooliveirafloyd@gmail.com>
2023-08-07 11:46:47 -0300

    Signed-off-by: DanFloyd2 <danilooliveirafloyd@gmail.com>

---
+++ classes_jogo/Level.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Level:
+    def __init__(self):
+        self.window = None
+        self.entity list = None
+        self.name = None
+        self.Attribute1 = None
+
+    def window(self, ):
+        pass
+
+    def run(self, ):
+        pass
+
+    def entity list(self, ):
+        pass
+
+    def name(self, ):
+        pass
+
+    def run(self, ):
+        pass
+
